1883f2b7dc2e98947399eef7847437091caf08c9
Anton Kurakov <anton_dev47@mail.ru>
2022-06-13 21:12:12 +0400

    folders structure changed

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Logo from "../Blocks/Logo"
+import Search from "../Blocks/Search";
 
 import {
     BrowserRouter as Router,
@@ -8,18 +10,19 @@ import {
     useParams
   } from "react-router-dom";
 
+
 function Header(){
     return (
         <div className="header">
             <div className="container-width">
-                <div className="logo">This is logo</div>
+                <Logo />
+                <Search />
 
-
-                <ul>
+                {/* <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/categories">Category</Link></li>
                     <li><Link to="/products/1">Product</Link></li>
-                </ul>
+                </ul> */}
             </div>
         </div>
     )
