d3e2bf92a33e654034a911bf78f7b19a1f569998
Anton Kurakov <anton_dev47@mail.ru>
2022-07-16 14:18:55 +0400

    shortlist component design changes

--- src/blocks/products_list_templates/ProductsShortList/ProductsShortList.js
+++ src/blocks/products_list_templates/ProductsShortList/ProductsShortList.js
@@ -15,24 +15,36 @@ function ProductsShortList({ product }){
 
                 <div className="shortlist__item-content">
                     <div className="shortlist__item-name">
-                        <Link to={"products/" + product.id}>{product.title}</Link>
+                        <Link className="shortlist__item-name-a" to={"products/" + product.id}>{product.title}</Link>
                     </div>
                     <div className="shortlist__item-rating">
-                        {product.rating}
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star-half-stroke"></i>
+                        <i className="fa-regular fa-star"></i>
+                        <div className="shortlist__item-rating-value">
+                            ({product.rating})
+                        </div>
                     </div>
-                    <div className="shortlist__item-stock">
-                        <span>В наличии:</span>
-                        <span>{product.stock}</span>
+                    <div className="shortlist__item-code">
+                        <span className="shortlist__item-code-label">КОД:</span>
+                        <span className="shortlist__item-code-value">AP534523466</span>
+                    </div>
+                    <div className={'shortlist__item-stock ' + ((product.stock > 0) ? 'shortlist__item-stock_active' : '')}>
+                        <i className={'fa-regular ' + ((product.stock > 0) ? 'fa-circle-check' : 'fa-circle-xmark')}></i>
+                        <span className='shortlist__item-stock-label'>В наличии:</span>
+                        <span className='shortlist__item-stock-value'>{product.stock} шт.</span>
                     </div>
                 </div>
 
                 <div className="shortlist__item-controls">
                     <div className="shortlist__item-price">
-                        {product.price}
+                        {product.price} руб.
                     </div>
-                    <div className="shortlist__item-addtocart">Купить</div>
-                    <div className="shortlist__item-addtowishlist">Добавить в желаемое</div>
-                    <div className="shortlist__item-addtocompare">Добавить в сравнение</div>
+                    <div className="shortlist__item-add_to_cart"><i className="fa-solid fa-cart-shopping"></i></div>
+                    <div className="shortlist__item-add_to_wishlist"><i class="fa-regular fa-heart"></i></div>
+                    <div className="shortlist__item-add_to_compare"><i className="fa-solid fa-chart-simple"></i></div>
                 </div>
             </div>
         </div>
--- src/blocks/static_templates/PaymentIcons/PaymentIcons.js
+++ src/blocks/static_templates/PaymentIcons/PaymentIcons.js
@@ -7,16 +7,16 @@ function PaymentIcons(){
     return (
         <div className="payment-icons">
             <div className="payment-icons__one">
-                <i class="fa-brands fa-cc-mastercard"></i>
+                <i className="fa-brands fa-cc-mastercard"></i>
             </div>
             <div className="payment-icons__one">
-                <i class="fa-brands fa-cc-paypal"></i>
+                <i className="fa-brands fa-cc-paypal"></i>
             </div>
             <div className="payment-icons__one">
-                <i class="fa-brands fa-cc-visa"></i>
+                <i className="fa-brands fa-cc-visa"></i>
             </div>
             <div className="payment-icons__one">
-                <i class="fa-brands fa-cc-stripe"></i>
+                <i className="fa-brands fa-cc-stripe"></i>
             </div>
         </div>
     )
