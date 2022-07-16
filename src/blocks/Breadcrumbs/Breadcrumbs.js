071a0047c4fe2d4fa73cacc9a03cae72e6d5b4b2
Anton Kurakov <anton_dev47@mail.ru>
2022-07-16 12:46:00 +0400

    breadcrumbs component

---
+++ src/blocks/Breadcrumbs/Breadcrumbs.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {
+    Link
+  } from "react-router-dom";
+
+import './Breadcrumbs.scss' 
+
+function Breadcrumbs(){
+    return (
+        <div className="breadcrumbs">
+            <Link className="breadcrumbs__link" to="/">Home</Link>
+            <span className="breadcrumbs__divider">/</span>
+            <Link className="breadcrumbs__link" to="/catalog">Catalog</Link>
+            <span className="breadcrumbs__divider">/</span>
+            <span className="breadcrumbs__active">Category</span>
+        </div>
+    )
+}
+
+export default Breadcrumbs
