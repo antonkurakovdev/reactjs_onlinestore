df0331b2d061fc5f2d801ccf3c9e35f09a860886
Anton Kurakov <anton_dev47@mail.ru>
2022-08-02 20:38:41 +0400

    added wishlist reducer

---
+++ src/redux/reducers/wishlistReducer.js
@@ -0,0 +1,18 @@
+import { ADD_WISHLIST_PRODUCT, REMOVE_WISHLIST_PRODUCT, CLEAR_WISHLIST } from "../types"
+
+const initialState = {
+    products: JSON.parse(localStorage.getItem("wishlist")) || []
+}
+
+export const wishlistReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_WISHLIST_PRODUCT:
+            return {...state, products: state.products.concat(action.data) }
+        case REMOVE_WISHLIST_PRODUCT:
+            return {...state, products: state.products.filter(obj => obj.id !== action.data.id) }
+        case CLEAR_WISHLIST:
+            return { products: [] }
+        default:
+           return state
+    }
+}
