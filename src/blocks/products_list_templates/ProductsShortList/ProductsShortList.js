432d5451ed5431ddae4847d16c64b5d15d278fe0
Anton Kurakov <anton_dev47@mail.ru>
2022-08-06 17:36:23 +0400

    remove from cart (short list)

--- src/blocks/products_list_templates/ProductsShortList/ProductsShortList.js
+++ src/blocks/products_list_templates/ProductsShortList/ProductsShortList.js
@@ -7,7 +7,7 @@ import { useDispatch } from "react-redux";
 import "./ProductsShortList.scss"
 
 import { updateProductAmount } from "../../../redux/actions/categoryActions";
-import { addToCart, updateCartTotal } from "../../../redux/actions/cartActions";
+import { addToCart, updateCartTotal, removeFromCart } from "../../../redux/actions/cartActions";
 import { addToWishlist, removeFromWishlist } from "../../../redux/actions/wishlistActions";
 import { addToCompare, removeFromCompare } from "../../../redux/actions/compareActions";
 
@@ -16,6 +16,7 @@ function ProductsShortList({ product, cart, wishlist, compare }){
     if (!product.amount){
         product.amount = 1;
     }
+    const productInCart = cart.products.filter(item => item.id === product.id)[0]
     const onClickAddToCart = () => {
         const item = {
             id: product.id,
@@ -27,6 +28,10 @@ function ProductsShortList({ product, cart, wishlist, compare }){
             category: product.category
         }
         dispatch(addToCart(item))
+        dispatch(updateCartTotal())
+    }
+    const onClickRemoveFromCart = () => {
+        dispatch(removeFromCart(product.id))
         dispatch(updateCartTotal())
     }
     const onClickAddToWishlist = () =>{
@@ -71,16 +76,21 @@ function ProductsShortList({ product, cart, wishlist, compare }){
         dispatch(updateProductAmount(product.id, parseInt(event.target.value)))
     }
 
-    let wishlistLink, compareLink;
-    if (wishlist.products.some((item) => item.id === product.id)){
-        wishlistLink = <div onClick={onClickRemoveFromWishlist} className="shortlist__item-add_to_wishlist"><i className="fa-solid fa-heart"></i></div>
+    let addToCartButton, wishlistLink, compareLink;
+    if (cart.products.some((item) => item.id === product.id)){
+        addToCartButton = <div onClick={onClickRemoveFromCart} className="shortlist__item-add_to_cart remove" title="Удалить из корзины"><i className="fa-solid fa-cart-shopping"></i><span className="count">{productInCart.amount}</span></div>
     }else{
-        wishlistLink = <div onClick={onClickAddToWishlist} className="shortlist__item-add_to_wishlist"><i className="fa-regular fa-heart"></i></div>
+        addToCartButton = <div onClick={onClickAddToCart} className="shortlist__item-add_to_cart" title="Добавить в корзину"><i className="fa-solid fa-cart-shopping"></i></div>
+    }
+    if (wishlist.products.some((item) => item.id === product.id)){
+        wishlistLink = <div onClick={onClickRemoveFromWishlist} className="shortlist__item-add_to_wishlist" title="Удалить из желаемого"><i className="fa-solid fa-heart"></i></div>
+    }else{
+        wishlistLink = <div onClick={onClickAddToWishlist} className="shortlist__item-add_to_wishlist" title="Добавить в желаемое"><i className="fa-regular fa-heart"></i></div>
     }
     if (compare.products.some((item) => item.id === product.id)){
-        compareLink = <div onClick={onClickRemoveFromCompare} className="shortlist__item-add_to_compare added"><i className="fa-solid fa-chart-simple"></i></div>
+        compareLink = <div onClick={onClickRemoveFromCompare} className="shortlist__item-add_to_compare added" title="Удалить из сравнения"><i className="fa-solid fa-chart-simple"></i></div>
     }else{
-        compareLink = <div onClick={onClickAddToCompare} className="shortlist__item-add_to_compare"><i className="fa-solid fa-chart-simple"></i></div>
+        compareLink = <div onClick={onClickAddToCompare} className="shortlist__item-add_to_compare" title="Добавить в сравнение"><i className="fa-solid fa-chart-simple"></i></div>
     }
     return (
         <div className="shortlist__item">
@@ -119,9 +129,7 @@ function ProductsShortList({ product, cart, wishlist, compare }){
                         {product.price} руб.
                     </div>
                     <div className="shortlist__item-add_to_cart-wrapper">
-                        <div onClick={onClickAddToCart} className="shortlist__item-add_to_cart">
-                            <i className="fa-solid fa-cart-shopping"></i>
-                        </div>
+                        {addToCartButton}
                         <div className="qty">
                             <div className="qty__wrapper">
                                 <span onClick={onClickDecreaseAmount} className="qty__decrease"><i className="fa-solid fa-minus"></i></span>
